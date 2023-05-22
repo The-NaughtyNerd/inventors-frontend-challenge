266c0f07a1e35e5946bf5c7d01cb63363f474dbe
David Ade <davemoneymachine@gmail.com>
2023-05-22 21:57:34 +0100

    [UPDATED]: navigation modified

--- script.js
+++ script.js
@@ -1,53 +1,27 @@
+"use strict";
+
 const hamburgerOpen = document.querySelector(".hamburger-open");
-const navbar = document.querySelector(".navbar");
 const hamburgerClose = document.querySelector(".hamburger-close");
-const smallNavbar = document.querySelector(".nav__bar-sm");
+const smallNavbar = document.querySelector(".nav-sm");
 
-// if (smallNavbar.style.display === "none") {
-//   hamburgerOpen.addEventListener("click", function () {
-//     smallNavbar.style.display = "contents";
-//     hamburgerOpen.style.display = "none";
-//     hamburgerClose.style.display = "block";
-//     console.log("clicked");
-//   });
-// } else if (smallNavbar.style.display === "contents") {
-//   hamburgerClose.addEventListener("click", function () {
-//     smallNavbar.style.display = "none";
-//     hamburgerClose.style.display = "none";
-//     hamburgerOpen.style.display = "block";
-//   });
-// }
-
+// HAMBURGER FUNCTIstyle.display = "block"ONALITY
 hamburgerOpen.addEventListener("click", () => {
   {
     smallNavbar.style.display = "contents";
-    hamburgerOpen.style.display = "none";
-    hamburgerClose.style.display = "block";
+    hamburgerOpen.classList.toggle("active");
+    hamburgerClose.classList.toggle("active");
     console.log("clicked");
   }
 });
 hamburgerClose.addEventListener("click", () => {
   {
     smallNavbar.style.display = "none";
-    hamburgerOpen.style.display = "inherit";
-    hamburgerClose.style.display = "none";
+    hamburgerOpen.classList.toggle("active");
+    hamburgerClose.classList.toggle("active");
     console.log("clicked");
   }
 });
 
-// hamburgerOpen.addEventListener('click', ()=> {
-//   if(smallNavbar.style.display === 'none'){
-//     smallNavbar.style.display = "contents";
-//     hamburgerOpen.style.display = "none";
-//     hamburgerClose.style.display = "block";
-//     console.log("clicked");
-//   }else {
-//     smallNavbar.style.display = "none";
-//     hamburgerOpen.style.display = "block";
-//     hamburgerClose.style.display = "none";
-//     console.log("clicked");
-//   }
-// })
 // const DUMMY_MOVIES = [
 //   {
 //     id: 1,
@@ -70,3 +44,31 @@ hamburgerClose.addEventListener("click", () => {
 // const data = DUMMY_MOVIES.map((movie) => (
 //   // <div key={movie.id}> {movie.title}</div>
 // ));
+
+/** Fetch data from Server */
+const apiKey = "5dc3bb994fb076d56fd9dd8942e2f25a";
+
+// const fetchData = function (url, callback, optionalParam) {
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => callback(data, optionalParam));
+// };
+
+let movieList;
+
+const fetchData = async () => {
+  try {
+    const res = await fetch(
+      "http://api.themoviedb.org/3/movie/popular?api_key=5dc3bb994fb076d56fd9dd8942e2f25a"
+    );
+    const data = await res.json();
+
+    movieList = data.results;
+    console.log(movieList);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+fetchData();
+console.log(movieList);
